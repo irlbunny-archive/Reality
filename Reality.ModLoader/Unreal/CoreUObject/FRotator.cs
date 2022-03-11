3077b7553dcfc22103e7998b6e87317c2f65ace9
Kaitlyn <87990853+ItsKaitlyn03@users.noreply.github.com>
2022-03-11 15:06:56 -0500

    Implement FVector and FRotator

---
+++ Reality.ModLoader/Unreal/CoreUObject/FRotator.cs
@@ -0,0 +1,25 @@
+﻿namespace Reality.ModLoader.Unreal.CoreUObject
+{
+    public class FRotator : UMemoryObject
+    {
+        public float Pitch
+        {
+            get => ReadSingle(0);
+            set => WriteSingle(0, value);
+        }
+
+        public float Yaw
+        {
+            get => ReadSingle(4);
+            set => WriteSingle(4, value);
+        }
+
+        public float Roll
+        {
+            get => ReadSingle(8);
+            set => WriteSingle(8, value);
+        }
+
+        public override int ObjectSize => 0xC;
+    }
+}
---
+++ Reality.ModLoader/Unreal/CoreUObject/FVector.cs
@@ -0,0 +1,25 @@
+﻿namespace Reality.ModLoader.Unreal.CoreUObject
+{
+    public class FVector : UMemoryObject
+    {
+        public float X
+        {
+            get => ReadSingle(0);
+            set => WriteSingle(0, value);
+        }
+
+        public float Y
+        {
+            get => ReadSingle(4);
+            set => WriteSingle(4, value);
+        }
+
+        public float Z
+        {
+            get => ReadSingle(8);
+            set => WriteSingle(8, value);
+        }
+
+        public override int ObjectSize => 0xC;
+    }
+}
